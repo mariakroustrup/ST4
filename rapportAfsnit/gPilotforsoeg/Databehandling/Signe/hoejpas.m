3c2411380f682b01bf0b69ff7a276eea5c480a1e
signehk <signehk@Signes-MacBook-Pro.local>
2016-04-26 17:08:13 +0200

    asdas

--- rapportAfsnit/gPilotforsoeg/Databehandling/Signe/hoejpas.m
+++ rapportAfsnit/gPilotforsoeg/Databehandling/Signe/hoejpas.m
@@ -28,34 +28,12 @@ legend('Ufiltreret', 'Filtreret')
 set(gca,'fontsize',20);
 box off
 
-%figure
-%subplot(2,1,1)
-%semilogy(f, m1P1_off)
-%xlabel('Frekvens (Hz)')
-%ylabel('Størrelse')
-%xlim([10^-1 10^2])
-%title('Ufiltreret')
-%set(gca,'fontsize',20);
-%box off
-
-%subplot(2,1,2)
-%semilogy(f, m2P1_off)
-%xlabel('Frekvens (Hz)')
-%ylabel('Størrelse')
-%xlim([10^-1 10^2])
-%title('Filtreret')
-%set(gca,'fontsize',20);
-%box off
-
 figure
 hold on
 plot(f, m1P1_off,'b')
 plot(f, m2P1_off,'g')
 xlabel('Frekvens (Hz)')
 ylabel('Størrelse')
-%xlim([10^-1 10^2])
-%ylim([0 10e-4])
-%set(gca,'ytick',[0,0.001, 0.01, 1]*10e-4)
 legend('Ufiltreret','Filtreret')
 set(gca,'fontsize',20);
 box off
