eff53099b1e45fdcb55610ce9bff3c58e01987cb
MadsKris12 <mail@mads-k.dk>
2016-05-11 12:37:32 +0200

    ADCCCCCCCCCCCCCC

--- PSoC/Det pisse meget rigtigt!/SimplePortReader_3kanaler.m
+++ PSoC/Det pisse meget rigtigt!/SimplePortReader_3kanaler.m
@@ -3,7 +3,7 @@ comport = 'COM8'; % Vælg porten til jeres egen PSoC enhed
 serial_port = serial(comport, 'TimeOut', 2, 'BaudRate', 115200);
 
 num_of_channels = 3;    % Antallet af kanaler
-data_length = 500;     % Antal samples per kanal i plottet
+data_length = 3000;     % Antal samples per kanal i plottet
 data = NaN*ones(data_length,num_of_channels);   % Initialisering af data
 byte_per_channel = 2;   % Antal bytes per kanal (int16 = 2 bytes)
 samples_per_channel = 22; % Antal samples per læsning fra porten
