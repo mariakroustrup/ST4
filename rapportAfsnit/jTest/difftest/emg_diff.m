27dab0b32e1c4e81ac6504cead0d6d0a26c03fe7
signehk <signehk@h468.aau1x-2.klient.frb7ab.site.aau.dk>
2016-05-13 16:14:01 +0200

    !!!

--- rapportAfsnit/jTest/difftest/emg_diff.m
+++ rapportAfsnit/jTest/difftest/emg_diff.m
@@ -5,16 +5,33 @@ T = 1/Fs;
 L = Fs*Samplelength;
 t = (0:L-1)*T;
 
-EMG=data(100:600,1);
-diff=data(100:600,2);
+EMG=data4(1:1000,1);
+diff=data4(1:1000,2);
+
+ymax = 1200;
+ymin = 0;
+imax = 4.8;
+imin = 0;
+
+ymax2 = 20;
+ymin2 = -20;
+imax2 = 4.8;
+imin2 = 0;
 
 hold on
-plot(t(1:501), EMG)
-plot(t(1:501), diff)
-xlim([0 5])
-ylim([-20 1200])
+plot(t(1:1000), EMG)
+[ymax,imax,ymin,imin] = extrema(EMG)
+hold on
+plot(t(imax),ymax,'r*',t(imin),ymin,'g*')
+plot(t(1:1000), diff)
+%[ymax2,imax2,ymin2,imin2] = extrema(diff);
+%hold on
+%plot(t(imax2),ymax2,'b*',t(imin2),ymin2,'y*')
+xlim([0 10])
+ylim([-400 1500])
+axis
 ylabel('Spænding [V]')
 xlabel('Tid [s]')
-legend('Input', 'Output')
+%legend('Input', 'Output')
 set(gca,'fontsize',20);
 box off
