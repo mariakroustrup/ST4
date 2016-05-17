c628bc70c4b0497125764816e99595ebff51c06a
Maria <mkrous14@student.aau.dk>
2016-05-17 20:23:18 +0200

    fgdf

--- rapportAfsnit/jTest/difftest/kontroltest.m
+++ rapportAfsnit/jTest/difftest/kontroltest.m
@@ -28,27 +28,17 @@ ymin = 0;
 imax = 10;
 imin = 0;
 
-% ymax2 = -10;
-% ymin2 = 0;
-% imax2 = 10;
-% imin2 = 0;
+ymax2 = -10;
+ymin2 = 0;
+imax2 = 10;
+imin2 = 0;
 
 
 figure
 plot(t(1:1000), sinus(1:1000));
-[ymax,imax,ymin,imin] = extrema(tempsinus)
+[ymax,imax,ymin,imin] = extrema(sinus)
 hold on
 plot(t(imax),ymax,'r*',t(imin),ymin,'g*')
-%plot(t(1:1000), diff)
-%[ymax2,imax2,ymin2,imin2] = extrema(diff);
-%hold on
-%plot(t(imax2),ymax2,'b*',t(imin2),ymin2,'y*')
-%xlim([0 10])
-%set(gca,'Xtick',[0,0.001, 0.01, 1])
-% ylim([-400 1500])
-% ylabel('Spænding [V]')
-% xlabel('Tid [s]')
-% legend('Input', 'Output')
-% set(gca,'fontsize',20);
-% box off
+
+
 
