28348cd2c5c8761bb70e2088439cdebe9854fdf3
signehk <signehk@h19.aau1x-2.klient.frb7ab.site.aau.dk>
2016-05-17 21:08:24 +0200

    jfghg

--- rapportAfsnit/jTest/difftest/emg_diff.m
+++ rapportAfsnit/jTest/difftest/emg_diff.m
@@ -5,10 +5,10 @@ T = 1/Fs;
 L = Fs*Samplelength;
 t = (0:L-1)*T;
 
-EMG=data(1:1000,1);
+EMG=data(1:1000,1)/10000;
 diff=data(1:1000,2);
 
-ymax = 1200;
+ymax = 1200/1000;
 ymin = 0;
 imax = 4.8;
 imin = 0;
@@ -18,19 +18,21 @@ ymin2 = -20;
 imax2 = 4.8;
 imin2 = 0;
 
+figure
 hold on
+[hAx,hLine1,hLine2] = plotyy(t(1:1000), samlet, t(1:1000), data1);
+
 plot(t(1:1000), EMG)
 [ymax,imax,ymin,imin] = extrema(EMG)
 hold on
-plot(t(imax),ymax,'r*',t(imin),ymin,'g*')
-plot(t(1:1000), diff)
-[ymax2,imax2,ymin2,imin2] = extrema(diff);
-hold on
-plot(t(imax2),ymax2,'b*',t(imin2),ymin2,'y*')
+plot(t(imax),ymax,'r*',t(imin),ymin,'g*');
+ylim([-0.1 0.2])
+ylabel('Spænding [V]')
+yyaxis right
+plot(t(1:1000), diff);
+ylim([-20 20])
 xlim([0 10])
 set(gca,'Xtick',[0,0.001, 0.01, 1])
-ylim([-400 1500])
-ylabel('Spænding [V]')
 xlabel('Tid [s]')
 legend('Input', 'Output')
 set(gca,'fontsize',20);
